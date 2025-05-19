06437ecc8cc5781ac332a142a51d51625fcc9f5e
JEONGMUN LEE <lot9616@gmail.com>
2025-05-19 20:30:41 +0900

    Update ScrollText.js

--- quizapp/src/components/common/ScrollText.js
+++ quizapp/src/components/common/ScrollText.js
@@ -14,7 +14,7 @@ function ScrollText({ children }) {
       <div ref={ref} className={`scroll-text ${inView ? "visible" : "hidden"}`}>
         <img src={noneMainLogo} alt="GENIUS GAME" className="main-logo" />
         <p className="signup-text" style={{textAlign: 'center', wordBreak: 'keep-all', marginTop: '15px'}}>
-          제 <span style={{color: '#d8c27c'}}>4게임</span> 이 곧 시작됩니다.
+          제 <span style={{color: '#d8c27c'}}>4게임</span> 에 참가해주셔서 감사합니다.
         </p>
         {/* <p className="signup-text" style={{textAlign: 'center', wordBreak: 'keep-all'}}>
           참여해주셔서 <span style={{color: '#d8c27c'}}>감사</span>합니다.
